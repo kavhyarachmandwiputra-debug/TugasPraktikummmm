fa6925e77b57ba7adc9fd6a701b6dbef41603c0c
KavhyaRachmanD <kavhyarachman123@gmail.com>
2025-10-08 14:18:59 +0700

    TugasPraktik

--- Tugas3/tugas3.java
+++ Tugas3/tugas3.java
@@ -4,39 +4,29 @@ import java.util.Scanner;
 public class tugas3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int kapasitasMaks = 10;
-        int[] antrean = new int[kapasitasMaks];
-        int jumlahDalamAntrean = 0;
+        int kapasitas = 10;
 
         System.out.print("Masukkan jumlah pasien yang datang: ");
         int jumlahPasien = input.nextInt();
 
-        System.out.println("\n--- Simulasi Antrean Klinik ---");
+        int pasienDilayani = 0;  // menghitung pasien yang sudah keluar antrean
+
+        System.out.println("\n=== Simulasi Antrean Klinik ===");
 
         for (int i = 1; i <= jumlahPasien; i++) {
-            if (jumlahDalamAntrean == kapasitasMaks) {
-                System.out.println("Antrian penuh. Pasien nomor " + antrean[0] + " telah dilayani.");
-
-                for (int j = 0; j < jumlahDalamAntrean - 1; j++) {
-                    antrean[j] = antrean[j + 1];
-                }
-
-                jumlahDalamAntrean--;
+            if (i - pasienDilayani > kapasitas) {
+                pasienDilayani++; // layani pasien pertama
+                System.out.println("Antrean penuh! Pasien nomor " + pasienDilayani + " selesai dilayani.");
             }
 
-            antrean[jumlahDalamAntrean] = i;
-            jumlahDalamAntrean++;
-
-            System.out.println("Pasien nomor " + i + " masuk ke antrean. (Total antrean: " + jumlahDalamAntrean + ")");
+            System.out.println("Pasien nomor " + i + " masuk antrean (total antrean: " + (i - pasienDilayani) + ")");
         }
 
         System.out.println("\nPasien yang masih dalam antrean:");
-        for (int i = 0; i < jumlahDalamAntrean; i++) {
-            System.out.print(antrean[i] + " ");
+        for (int i = pasienDilayani + 1; i <= jumlahPasien; i++) {
+            System.out.println("Pasien nomor " + i);
         }
 
         input.close();
     }
 }
-
-
